ac6f1ca22efbecc00425961830f1c925f758865f
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-03 19:34:01 +0430

    feat: build network method in nn

--- network/network.py
+++ network/network.py
@@ -12,7 +12,25 @@ class NeuralNetwork:
         @argument input_size is the size of our neural network
     """
     def __init__(self, size: list):
+        # alocating the network size
         self.size = size
+        # creating our network architecture
+        self.parameters = self.__build_network()
+    
+    """
+    build network
+        @returns parameters of our network architecture
+    """
+    def __build_network(self) -> dict:
+        parameters = {}
+
+        for le in range(1, len(self.layer_sizes)):  # number of layers in the network
+            parameters['W' + str(le)] = np.random.normal(
+                size=(self.layer_sizes[le], self.layer_sizes[le - 1])
+            )
+            parameters['b' + str(le)] = np.zeros((self.layer_sizes[le], 1))
+
+        return parameters
     
     def activation_function(self, x):
         pass
