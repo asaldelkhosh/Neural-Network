5203e64945a70afe7f54da2206a0187e6c33896c
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-11 09:53:56 +0430

    add: fit test

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ DATA_SET = 'xor'
 
 if __name__ == "__main__":
     # dataset
-    x = []
-    y = []
+    X = []
+    labels = []
 
     # loading dataset X
     path = './' + MAIN_DIR + '/' + DATA_SET + '/'
@@ -30,7 +30,7 @@ if __name__ == "__main__":
             temp = []
             temp.append(int(parts[0]))
             temp.append(int(parts[1]))
-            x.append(temp)
+            X.append(temp)
     # loading dataset labels
     with open(path + OT_FILES) as file:
         lines = file.readlines()
@@ -38,6 +38,24 @@ if __name__ == "__main__":
         for line in lines:
             if line == "" or line == "\n":
                 continue
-            y.append(int(line))
+            labels.append(int(line))
     
-    print(x, y)
+    X, labels = np.asarray(X), np.asarray(labels)
+    print(len(X), len(labels))
+
+    # test and train data
+    x_train, y_train = X[: int(len(X) / 2)], labels[: int(len(labels) / 2)]
+    x_test, y_test = X[int(len(X) / 2) :], labels[int(len(labels) / 2) :]
+
+    # define our 2-2-1 neural network and train it
+    nn = NeuralNetwork([2, 2, 1], alpha=0.5)
+    nn.fit(x_train, y_train)
+
+    # now that our network is trained, loop over the XOR data points
+    for (x, target) in zip(x_test, y_test):
+        # make a prediction on the data point and display the result
+        # to our console
+        pred = nn.predict(x)[0][0]
+        step = 1 if pred > 0.5 else 0
+        print("[INFO] data={}, ground-truth={}, pred={:.4f}, step={}".format(
+            x, target[0], pred, step))
